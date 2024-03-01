5bdfcd4e7eb9947516b8cf03b60e65aaf015840b
Fadwa Ali <fadwa.ali@elevoro.com>
2024-03-01 16:34:32 +0300

    setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#1D1D1D",
+        white: "#FFFFFF",
+        greency: "#28F3C3",
+        whitePro: "#F9F9F9",
+      },
+    },
   },
   plugins: [],
 };
